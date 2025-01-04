f99caff3b5a2000f47420b7f39de50ca51430477
computerguybinary <yassineishere321@gmail.com>
2025-01-04 20:06:26 +0100

    maybe good copy update

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function copy_text(){
     let text_to_copy = document.getElementById("code");
     text_to_copy.select()
     text_to_copy.setSelectionRange(0,99999);
-    navigator.clipboard.writeText(text_to_copy.value);
+    navigator.clipboard.writeText(text_to_copy.value);  
 }
